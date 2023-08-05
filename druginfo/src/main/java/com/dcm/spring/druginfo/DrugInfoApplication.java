2921e212ab89b80d1d88cc9d58bed5684896c783
suhanlim <soohan2009@naver.com>
2023-08-06 02:35:23 +0900

    FEAT: 스프링 부트 프로젝트 생성
    - 초기 스프링 부트 프로젝트 생성

---
+++ druginfo/src/main/java/com/dcm/spring/druginfo/DrugInfoApplication.java
@@ -0,0 +1,13 @@
+package com.dcm.spring.druginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugInfoApplication.class, args);
+	}
+
+}
