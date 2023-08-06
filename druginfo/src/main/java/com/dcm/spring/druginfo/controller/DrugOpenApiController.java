81b99e940a0bea730af6bad24eea74505e2597f9
suhanlim <soohan2009@naver.com>
2023-08-06 15:17:16 +0900

    FEAT: 의약품 이름으로 호출 기능 추가
    - 기존에 단순히 요청만을 보내던 코드에서 의약품의 이름을 통해 호출하도록 코드를 개선하였습니다.

--- druginfo/src/main/java/com/dcm/spring/druginfo/controller/DrugOpenApiController.java
+++ druginfo/src/main/java/com/dcm/spring/druginfo/controller/DrugOpenApiController.java
@@ -2,21 +2,25 @@ package com.dcm.spring.druginfo.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 public class DrugOpenApiController {
     private final static String BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
     private final String API_KEY = "2SKRXQ8IZz8FeCOyzzs8hgyk0YWybv0fp5wAMHnsnrgPTiEqwl%2FxxQSs%2BZMHxjUSUxZWIQ37pEh9zUjkqh9zlg%3D%3D";
 
-    @GetMapping(value = "/publicData", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getDrugInfo() {
+    @GetMapping(value = "/PublicData/{itemName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> getDrugInfo(@PathVariable String itemName) {
         DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
         factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
-
+        String encodedItemName = URLEncoder.encode(itemName, StandardCharsets.UTF_8);
         WebClient webClient = WebClient.builder()
                 .uriBuilderFactory(factory)
                 .baseUrl(BASE_URL)
@@ -28,7 +32,7 @@ public class DrugOpenApiController {
                         .queryParam("pageNo", "1")
                         .queryParam("numOfRows", "1")
                         .queryParam("entpName", "")
-                        .queryParam("itemName", "")
+                        .queryParam("itemName", encodedItemName)
                         .queryParam("itemSeq", "")
                         .queryParam("efcyQesitm", "")
                         .queryParam("useMethodQesitm", "")
