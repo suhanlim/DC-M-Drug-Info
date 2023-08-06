5bcc18b04e4d0b8e305ba544bcab0444b0b60e2d
suhanlim <soohan2009@naver.com>
2023-08-06 15:13:57 +0900

    FEAT: 공공포털 api 요청 기능 추가
    - 발급받은 api 키를 활용해 RestApi를 사용하여 데이터를 가져오는 Controller 코드를 추가하였습니다.

---
+++ druginfo/src/main/java/com/dcm/spring/druginfo/controller/DrugOpenApiController.java
@@ -0,0 +1,49 @@
+package com.dcm.spring.druginfo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class DrugOpenApiController {
+    private final static String BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
+    private final String API_KEY = "2SKRXQ8IZz8FeCOyzzs8hgyk0YWybv0fp5wAMHnsnrgPTiEqwl%2FxxQSs%2BZMHxjUSUxZWIQ37pEh9zUjkqh9zlg%3D%3D";
+
+    @GetMapping(value = "/publicData", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<String> getDrugInfo() {
+        DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(BASE_URL);
+        factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
+
+        WebClient webClient = WebClient.builder()
+                .uriBuilderFactory(factory)
+                .baseUrl(BASE_URL)
+                .build();
+
+        Mono<String> response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("serviceKey", API_KEY)
+                        .queryParam("pageNo", "1")
+                        .queryParam("numOfRows", "1")
+                        .queryParam("entpName", "")
+                        .queryParam("itemName", "")
+                        .queryParam("itemSeq", "")
+                        .queryParam("efcyQesitm", "")
+                        .queryParam("useMethodQesitm", "")
+                        .queryParam("atpnWarnQesitm", "")
+                        .queryParam("atpnQesitm", "")
+                        .queryParam("intrcQesitm", "")
+                        .queryParam("seQesitm", "")
+                        .queryParam("depositMethodQesitm", "")
+                        .queryParam("openDe", "")
+                        .queryParam("updateDe", "")
+                        .queryParam("type", "json")
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
+
+        return response;
+    }
+}
